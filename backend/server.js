2b2665d22a089a14509e9db5f9e8543e9afb488e
Ted Larsson <ted@pop-os.localdomain>
2020-07-15 09:09:26 +0200

    adding backend and frontend basics

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const url = process.env.ATLAS_URL;
+const HttpError = require('./models/http-error')
+
+const app = express();
+
+const port = process.env.PORT || 8090;
+
+//MIDDLEWARES
+app.use(express.json());
+
+app.use('/api/');
+
+app.use((req,res,next) => {
+  const error = new HttpError('could not find this route', 404)
+  return next(error)
+})
+
+app.use((error,req,res,next) => {
+  if(res.headerSent){
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({message: error.message || 'an unknown error occured'})
+});
+
+mongoose
+.connect(url, { 
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,
+  useFindAndModify: false})
+.then(() => {
+  app.listen(port, () =>{
+    console.log(`server is running on port: ${port}`)
+  });
+  console.log('Connected to database')
+})
+.catch( err => {
+  console.log(err)
+})
